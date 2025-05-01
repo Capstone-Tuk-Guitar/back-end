5ff524023ffbe01c576d59d0c6b10604ebde3700
jaepil <jeapil13@naver.com>
2025-05-01 10:41:07 +0900

    merge

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from function.mxlConverter import mxl_router
 from function.db_run import db_run_router
 from fastapi.staticfiles import StaticFiles
 from function.compare_detail import compare_detail_router
+from function.chordprac import chordprac_router
 import os
 
 app = FastAPI()
@@ -29,3 +30,4 @@ app.include_router(tuner_router)
 app.include_router(mxl_router)
 app.include_router(db_run_router)
 app.include_router(compare_detail_router)
+app.include_router(chordprac_router)
